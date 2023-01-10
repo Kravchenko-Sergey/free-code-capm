ac900bc1776026de7f4184d2a38da76fd381e305
Kravchenko-Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-01-10 22:53:35 +0300

    Unfinished Loop - Bug Fixing #1

---
+++ codeWars.js
@@ -0,0 +1,9 @@
+const createArray = number => {
+    const newArray = []
+    for(let counter = 1; counter <= number; counter++){
+        newArray.push(counter)
+    }
+    return newArray
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
