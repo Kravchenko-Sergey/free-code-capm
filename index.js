f20e1671aad0140d668aa6243bec9bbbe082b09e
Kravchenko-Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-01-10 22:16:26 +0300

    Return Largest Numbers in Arrays

--- index.js
+++ index.js
@@ -34,3 +34,15 @@ function findLongestWordLength(str) {
 findLongestWordLength("The quick brown fox jumped over the lazy dog")
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const largestOfFour = arr => {
+    const res = []
+    for(let i = 0; i < arr.length; i++) {
+        res.push(Math.max(...arr[i]))
+    }
+    return res
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
