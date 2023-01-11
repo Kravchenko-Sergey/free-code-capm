ac026aeae5611025fcf6e0b0f10a320ba1dc5e3c
Kravchenko-Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-01-11 15:44:27 +0300

    Title Case a Sentence

--- index.js
+++ index.js
@@ -97,3 +97,13 @@ const booWho = bool => bool === true || bool === false ? true : false
 booWho(null)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const titleCase = str => {
+    return str.split(' ').map(word => {
+        return `${word.slice(0, 1).toUpperCase()}${word.slice(1).toLowerCase()}`
+    }).join(' ')
+}
+
+titleCase("I'm a little tea pot")
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
