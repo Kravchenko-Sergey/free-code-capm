7641fa75d1e45c4f17c3370f1fb0e557c79b897d
Kravchenko-Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-01-12 15:10:10 +0300

    Use the delete Keyword to Remove Object Properties

--- index.js
+++ index.js
@@ -190,3 +190,20 @@ const checkInventory = scannedItem => foods[scannedItem]
 console.log(checkInventory("apples"))
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+}
+
+delete foods.oranges
+delete foods.plums
+delete foods.strawberries
+
+console.log(foods)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
