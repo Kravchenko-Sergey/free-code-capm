7bf30497df13a87e8c8153f6a48fb390092299d3
Kravchenko-Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-01-12 15:56:32 +0300

    Modify an Array Stored in an Object

--- index.js
+++ index.js
@@ -289,3 +289,32 @@ const getArrayOfUsers = obj => Object.keys(obj)
 console.log(getArrayOfUsers(users))
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+        username: 'kennethCodesAllDay',
+        joinDate: 'March 26, 2016',
+        organization: 'freeCodeCamp',
+        friends: [
+            'Sam',
+            'Kira',
+            'Tomo'
+        ],
+        location: {
+            city: 'San Francisco',
+            state: 'CA',
+            country: 'USA'
+        }
+    }
+}
+
+const addFriend = (userObj, friend) => {
+    userObj.data.friends.push(friend)
+    return userObj.data.friends
+}
+
+console.log(addFriend(user, 'Pete'))
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
