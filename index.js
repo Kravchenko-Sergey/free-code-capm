be33e1e8ca744093bd1fc548936ab26e2134c69f
Kravchenko-Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-01-12 15:19:44 +0300

    Check if an Object has a Property

--- index.js
+++ index.js
@@ -207,3 +207,36 @@ delete foods.strawberries
 console.log(foods)
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let users = {
+    Alan: {
+        age: 27,
+        online: true
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: true
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+}
+
+const isEveryoneHere = userObj => {
+    if('Alan' in userObj
+        && 'Jeff' in userObj
+        && 'Sarah' in userObj
+        && 'Ryan' in userObj) {
+        return true
+    }
+    return false
+}
+
+console.log(isEveryoneHere(users))
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
