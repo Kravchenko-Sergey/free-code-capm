cb9aadfb9e4c2780d083dc5d8c721c75bf0a0d73
Kravchenko-Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-01-12 15:06:20 +0300

    Access Property Names with Bracket Notation

--- index.js
+++ index.js
@@ -173,3 +173,20 @@ const chunkArrayInGroups = (arr, size) => {
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+}
+
+const checkInventory = scannedItem => foods[scannedItem]
+
+console.log(checkInventory("apples"))
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
