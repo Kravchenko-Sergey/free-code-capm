47e7b2a8b91df62ca14c25e5ae10190e1d5d94ac
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-09-08 21:45:21 +0300

    Equal Sides Of An Array

--- codeWars.js
+++ codeWars.js
@@ -175,3 +175,19 @@ export function golfScoreCalculator(parList, scoreList) {
 
     return totalScore;
 }
+
+// Equal Sides Of An Array
+
+export function findEvenIndex(arr)
+{
+    for (let i = 0; i < arr.length; i++) {
+        const leftSum = arr.slice(0, i).reduce((sum, num) => sum + num, 0);
+        const rightSum = arr.slice(i + 1).reduce((sum, num) => sum + num, 0);
+
+        if (leftSum === rightSum) {
+            return i;
+        }
+    }
+
+    return -1;
+}
