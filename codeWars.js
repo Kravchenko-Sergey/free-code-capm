9e7f2e18ce94af1fb4300f2a60f1a4c1fab8b46d
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-09-30 23:31:52 +0300

    A Rule of Divisibility by 7

--- codeWars.js
+++ codeWars.js
@@ -350,3 +350,18 @@ export function incrementer(nums) {
 
     return result;
 }
+
+// A Rule of Divisibility by 7
+
+export function seven(m) {
+    let steps = 0;
+
+    while (m >= 100) {
+        const y = m % 10;
+        const x = Math.floor(m / 10);
+        m = x - 2 * y;
+        steps++;
+    }
+
+    return [m, steps];
+}
